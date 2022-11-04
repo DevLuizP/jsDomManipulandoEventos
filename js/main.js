cd8b96a0dc82ab8cce02512ea2ddfe1f87563cc9
Luiz Paulo Cardoso <70473328+DevLuizP@users.noreply.github.com>
2022-11-03 23:08:57 -0300

    inital commit: ajuste na utilização dos métodos

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
+const resultStr = document.getElementById('text');
 const fuiClicado = document.getElementById('calcular');
+const resultNum = document.getElementById('contar');
+let count = 0;
 
-fuiClicado.addEventListener("click", (event) => {
-    console.log(event)
-    console.log(notasEnviadas())
+fuiClicado.addEventListener("click", () => {
+    let msg = "Fuic clicado: "
+    resultStr.innerText = msg
+    count++;
+    let qtd = count > 1 ? 'vezes' : 'vez';
+    resultNum.innerText = `${count} ${qtd}`;
 } );
-
-function notasEnviadas () {
-    return "Notas enviadas com sucesso!"
-}
